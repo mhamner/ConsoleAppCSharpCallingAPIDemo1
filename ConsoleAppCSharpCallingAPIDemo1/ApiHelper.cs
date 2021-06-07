eb5f4fe33cc49bfc61d27e8fdea367a584af1671
mhamner <37221769+mhamner@users.noreply.github.com>
2021-06-07 16:29:57 -0400

    Add files via upload

---
+++ ConsoleAppCSharpCallingAPIDemo1/ApiHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace ConsoleAppCSharpCallingAPIDemo1
+{
+    public class ApiHelper
+    {
+        public static HttpClient ApiClient { get; set; }
+
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+
+            //Clear any existing headers
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+
+            //Add a header saying "Our response is going to be in JSON"
+            ApiClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
---
+++ ConsoleAppCSharpCallingAPIDemo1/BusinessLogic/TvShowBL.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using ConsoleAppCSharpCallingAPIDemo1.Models;
+
+
+namespace ConsoleAppCSharpCallingAPIDemo1.BusinessLogic
+{
+    public class TvShowBL
+    {
+        public async Task<List<TvShowModel>> LoadTvShowInfo(string tvShowName)
+        {
+            string url = $"http://api.tvmaze.com/search/shows?q={tvShowName}";
+            using(HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if(response.IsSuccessStatusCode)
+                {
+                    var settings = new JsonSerializerSettings
+                    {
+                        NullValueHandling = NullValueHandling.Ignore,
+                        MissingMemberHandling = MissingMemberHandling.Ignore
+                    };
+                    var jsonString = response.Content.ReadAsStringAsync().Result;
+                    List<TvShowModel> tvShowInfo = JsonConvert.DeserializeObject<List<TvShowModel>>(jsonString, settings);
+
+                    //List<TvShowModel> tvShowInfo = await response.Content.ReadAsAsync<List<TvShowModel>>();
+                    return tvShowInfo;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                } 
+            }
+        }
+    }
+}
